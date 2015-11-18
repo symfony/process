7609cbcbc4246c55f97d362f493ff4eb11f2c2b6
Maxence Bourquin <maxence@carvinglabs.com>
2015-11-18 16:44:51 +0100

    Change PHP excutable find order (and handle PHP_PATH options)

--- PhpExecutableFinder.php
+++ PhpExecutableFinder.php
@@ -40,17 +40,20 @@ class PhpExecutableFinder
             return (getenv('PHP_BINARY') ?: PHP_BINARY).($includeArgs ? ' '.implode(' ', $this->findArguments()) : '');
         }
 
-        // PHP_BINARY return the current sapi executable
-        if (defined('PHP_BINARY') && PHP_BINARY && in_array(PHP_SAPI, array('cli', 'cli-server')) && is_file(PHP_BINARY)) {
-            return PHP_BINARY;
-        }
-
+        // Check PHP PATH environment variable
         if ($php = getenv('PHP_PATH')) {
-            if (!is_executable($php)) {
+
+            // check if PHP_PATH is executable (even with options ex: path/to/php -c something)
+            if (!is_executable(substr($php, 0, strrpos($php, ' ')))){
                 return false;
             }
 
             return $php;
+        }
+
+        // PHP_BINARY return the current sapi executable
+        if (defined('PHP_BINARY') && PHP_BINARY && in_array(PHP_SAPI, array('cli', 'cli-server')) && is_file(PHP_BINARY)) {
+            return PHP_BINARY;
         }
 
         if ($php = getenv('PHP_PEAR_PHP_BIN')) {
